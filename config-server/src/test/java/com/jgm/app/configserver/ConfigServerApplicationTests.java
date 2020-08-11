dd47df5d38f318dba65cb2b1dda40a34cf523905
jgalanmarcos <jgalanmarcos@gmail.com>
2020-08-11 10:29:37 +0200

    added config server

---
+++ config-server/src/test/java/com/jgm/app/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgm.app.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- servicio-items/src/main/java/com/jgm/app/servicioitems/infrastructure/controllers/ItemController.java
+++ servicio-items/src/main/java/com/jgm/app/servicioitems/infrastructure/controllers/ItemController.java
@@ -6,13 +6,21 @@ import com.jgm.app.servicioitems.domain.services.ItemServiceI;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@RefreshScope
 @RestController
 @RequestMapping("/items")
 public class ItemController {
@@ -20,6 +28,15 @@ public class ItemController {
     @Autowired
     @Qualifier("itemServiceImplFeign")
     private ItemServiceI itemServiceI;
+
+    @Value("${configuracion.texto}")
+    private String texto;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping
     public List<Item> findAll() {
@@ -44,6 +61,20 @@ public class ItemController {
         item.setCantidad(cantidad);
 
         return item;
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<?> getConfig() {
+        Map<String, String> map = new HashMap<>();
+        map.put("texto", texto);
+        map.put("port", port);
+
+        if (environment.getActiveProfiles().length > 0 && environment.getActiveProfiles()[0].equals("dev")) {
+            map.put("autor.nombre", environment.getProperty("configuracion.autor.nombre"));
+            map.put("autor.email", environment.getProperty("configuracion.autor.email"));
+        }
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
 }
